52e73bceeebc182edc9c2857997852b574c0167f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-11-04 19:07:30 -0500

    form created

--- registration.php
+++ registration.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Registration Form</title>
 </head>
 <body>
-    helol
+    <div class="container">
+        <form action="registration.php" method="post">
+        <div class="form-group">
+            <input type="text" name="Fullname" placeholder="Full Name:">
+        </div>
+        <div class="form-group">
+            <input type="email" name="email" placeholder="Email:">
+        </div>
+        <div class="form-group">
+            <input type="Password" name="Password" placeholder="Password:">
+        </div>
+        <div class="form-group">
+            <input type="text" name="repeat_password" placeholder="Repeat Password">
+        </div>
+        <div class="form-group">
+            <input type="submit" name="Register" placeholder="submit">
+        </div>
+        </form>
+    </div>
+    
 </body>
 </html>
